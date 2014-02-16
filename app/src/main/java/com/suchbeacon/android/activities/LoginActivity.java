3daea72556fa8113ebcd287b16ef1d8a6e63bfa7
David Carr <dcarr@scienceprousa.com>
2014-02-16 00:48:41 -0500

    button enable/disable behavior on loginactivity

--- app/src/main/java/com/suchbeacon/android/activities/LoginActivity.java
+++ app/src/main/java/com/suchbeacon/android/activities/LoginActivity.java
@@ -6,6 +6,7 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -16,6 +17,7 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.google.android.gms.auth.GoogleAuthException;
 import com.google.android.gms.auth.GoogleAuthUtil;
@@ -63,15 +65,58 @@ public class LoginActivity extends Activity {
                 startActivityForResult(intent, ACCOUNT_PICK_REQUEST_CODE);
             }
         });
+    }
 
-        findViewById(R.id.next_button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent mainIntent = new Intent(mContext, MainActivity.class);
-                startActivity(mainIntent);
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.d(TAG, "onActivityResult " + resultCode);
+//        if (resultCode == VENMO_OK) {
+//            ImageView gPlusCheck = (ImageView) findViewById(R.id.check_box_venmo);
+//            gPlusCheck.setVisibility(View.VISIBLE);
+//        }
+        if (requestCode == ACCOUNT_PICK_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+
+                ImageView gPlusCheck = (ImageView) findViewById(R.id.check_box_gplus);
+                gPlusCheck.setVisibility(View.VISIBLE);
+                findViewById(R.id.google_plus_sign_in_button).setOnClickListener(null);
+
+                Button next = (Button) findViewById(R.id.next_button);
+                next.setBackground(getResources().getDrawable(R.drawable.rounded_rectangle_offwhite));
+                next.setTextColor(Color.BLACK);
+                setNextListener();
+
+                LinearLayout venmo = (LinearLayout) findViewById(R.id.venmo_sign_in_button);
+                venmo.setBackground(getResources().getDrawable(R.drawable.rounded_rectangle_blue));
+                setVenmoClickListener();
+
+                String accountName = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
+                sharedPrefs.edit().putString("account", accountName).commit();
+
+                new AsyncTask<Void, Void, Void>() {
+
+                    @Override
+                    protected Void doInBackground(Void... voids) {
+                        try {
+                            Log.d("scope", Constants.SCOPE);
+                            Log.d("token", GoogleAuthUtil.getToken(LoginActivity.this, sharedPrefs.getString("account", null), Constants.SCOPE));
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } catch (UserRecoverableAuthException e) {
+                            startActivityForResult(e.getIntent(), REQUEST_AUTHORIZATION);
+                        } catch (GoogleAuthException e) {
+                            e.printStackTrace();
+                        }
+                        return null;
+                    }
+                }.execute();
+
             }
-        });
+        } else
+            super.onActivityResult(requestCode, resultCode, data);
+    }
 
+    private void setVenmoClickListener() {
         findViewById(R.id.venmo_sign_in_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -107,48 +152,15 @@ public class LoginActivity extends Activity {
                 alert.show();
             }
         });
-
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        Log.d(TAG, "onActivityResult " + resultCode);
-//        if (resultCode == VENMO_OK) {
-//            ImageView gPlusCheck = (ImageView) findViewById(R.id.check_box_venmo);
-//            gPlusCheck.setVisibility(View.VISIBLE);
-//        }
-        if (requestCode == ACCOUNT_PICK_REQUEST_CODE) {
-            if (resultCode == RESULT_OK) {
-
-                ImageView gPlusCheck = (ImageView) findViewById(R.id.check_box_gplus);
-                gPlusCheck.setVisibility(View.VISIBLE);
-
-                Button next = (Button) findViewById(R.id.next_button);
-                next.setVisibility(View.VISIBLE);
-
-                String accountName = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
-                sharedPrefs.edit().putString("account", accountName).commit();
-
-                new AsyncTask<Void, Void, Void>() {
-
-                    @Override
-                    protected Void doInBackground(Void... voids) {
-                        try {
-                            Log.d("scope", Constants.SCOPE);
-                            Log.d("token", GoogleAuthUtil.getToken(LoginActivity.this, sharedPrefs.getString("account", null), Constants.SCOPE));
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        } catch (UserRecoverableAuthException e) {
-                            startActivityForResult(e.getIntent(), REQUEST_AUTHORIZATION);
-                        } catch (GoogleAuthException e) {
-                            e.printStackTrace();
-                        }
-                        return null;
-                    }
-                }.execute();
-
+    private void setNextListener() {
+        findViewById(R.id.next_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mainIntent = new Intent(mContext, MainActivity.class);
+                startActivity(mainIntent);
             }
-        } else
-            super.onActivityResult(requestCode, resultCode, data);
+        });
     }
 }
